9b897adc390891b7b8740ea6dcf8bcfa9ebb26b9
JRIX-DESKTOP-2\jrixg <jrixgeromo@gmail.com>
2024-10-03 07:09:24 +0800

    web app intigration more progress

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,68 +1,73 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.web.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    alias: {
-      'react-native$': 'react-native-web',
-      'react-native-vector-icons$': 'react-native-vector-icons/dist/index.js', // Handle react-native-vector-icons
+    entry: './index.web.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-    extensions: ['.web.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: 
-          {
-            "presets": [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript"
-            ],
-              "plugins": [
-                ["@babel/plugin-transform-class-properties", { "loose": true }],
-                ["@babel/plugin-transform-private-methods", { "loose": true }],
-                ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
-            ]
-          }
-        },
+    resolve: {
+      alias: {
+        'react-native$': 'react-native-web',
+        'react-native-vector-icons$': 'react-native-vector-icons/dist/index.js', // Handle react-native-vector-icons
       },
-      {
-        test: /\.js$/, // Handle normal JavaScript files
-        exclude: /node_modules\/(?!(react-native-vector-icons|@react-native-community\/datetimepicker)\/).*/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+      extensions: ['.web.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+              ],
+              plugins: [
+                ['@babel/plugin-transform-class-properties', { loose: true }],
+                ['@babel/plugin-transform-private-methods', { loose: true }],
+                ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: {
-          loader: 'file-loader',
+        {
+          test: /\.js$/, // Handle normal JavaScript files
+          exclude: /node_modules\/(?!(react-native-vector-icons|@react-native-community\/datetimepicker)\/).*/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: [
+                ['@babel/plugin-transform-class-properties', { loose: true }],
+                ['@babel/plugin-transform-private-methods', { loose: true }],
+                ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+              ],
+            },
+          },
         },
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          use: {
+            loader: 'file-loader',
+          },
+        },
+      ],
     },
-    compress: true,
-    port: 9000,
-  },
-  mode: 'development', // Set to 'development' for local development
-};
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 9000,
+    },
+    mode: 'development', // Set to 'development' for local development
+  };
+  
